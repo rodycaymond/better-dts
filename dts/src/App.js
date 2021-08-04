d8281d7adab8bd25e96eac3fe28fb8b73051189e
Cody Raymond <codyraymond@rocketmail.com>
2021-08-04 15:18:34 -0400

    new trip window built, not yet functional

--- dts/src/App.js
+++ dts/src/App.js
@@ -1,21 +1,26 @@
+//Style imports
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { useState } from 'react';
+
+//React imports
+import React, { useState } from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+//Material UI imports
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import React from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import {
-  Card,
-  CardActions,
-  CardHeader,
-  CardMedia,
-} from '@material-ui/core';
-import WindowModal from 'react-window-modal';
+import { Card, CardActions, CardHeader, CardMedia } from '@material-ui/core';
+
+//Custom Components
 import PreviousTrips from './components/PreviousTrips.js';
 import NewTrip from './components/NewTrip.js';
-// import Paper from '@material-ui/core/Paper';
-// import { styled } from '@material-ui/core/styles';
+
+//Not used imports
+/*
+import WindowModal from 'react-window-modal';
+
+
+*/
 
 
 
@@ -24,15 +29,20 @@ function App() {
   const [isNewTrip, setNewTrip] = useState(false)
   // setting state for whether we are adding newtrips or not to open modal
   const [previousTripsArray, setPreviousTrips] = useState([])
+  const [tripData, setTripData] = useState()
   // setting state for previous trips
 
   const useStyles = makeStyles({
     appbar:{display: 'flex', position: 'static', padding: '10px 0px',  justifyContent: 'space-around', flexDirection: 'row'},
-    button: {width: '40%', border: 'black', background:'orange'},
+    button: {width: '40%', border: 'black', background:'orange', textDecoration: 'none'},
     title: {width: '40%', padding: '10px', color: 'whitesmoke', backgroundColor: 'rgba(63,81,181,0)', boxShadow: '0 0 0'}
   })
   // lines above in useStyles create an object to be invoked by the classes variable below. the object keys are placed in the rendering of the html/jsx tags below to apply the styling
   const classes = useStyles();
+
+  function handleClick(){
+    console.log(tripData);
+  }
   return (
       <div className="App">
         <AppBar className={classes.appbar}>
@@ -40,24 +50,17 @@ function App() {
             <Link to='/newTrip'>
               <Button className={classes.button}>New Trip</Button>
             </Link>
+            <Link to='/'>
+              <Button className={classes.button}>Home</Button>
+            </Link>
         </AppBar>
-        <Route exact path='/' component={PreviousTrips}/>
-        <Route exact path='/newTrip' component={NewTrip}/>
+        <div>
+          <Route exact path='/' component={PreviousTrips}/>
+          <Route exact path='/newTrip' component={props => <NewTrip setTripData={setTripData}/>}/>
+        </div>
+        {/* <button onClick={handleClick}>See trip data</button> */}
       </div>
   );
 }
-
-// const MyButton = styled(Button)({
-//   background: 'linear-gradient(90deg, #44FA34 30%, #FE45EE 70%)',
-//   border: 0,
-//   borderRadius: 50,
-//   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-//   color: 'Goldenrod',
-//   fontSize: 36,
-//   height: 72,
-//   padding: '100px 50px',
-//   //padding 'x is heightpx y is heightpx
-// });
-
 
 export default App;
--- dts/src/components/NewTrip.js
+++ dts/src/components/NewTrip.js
@@ -1,10 +1,47 @@
-import WindowModal from 'react-window-modal';
+import React, { useState, useHistory } from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
 
-function NewTrip () {
+
+function NewTrip ({setTripData}) {
+  const [destination, setDestination] = useState('');
+  const [depLoc, setDepLoc] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [returnDate, setReturnDate] = useState('');
+//useState above is using hooks to hold information
+  
+  
+  function handleClick(event){
+    event.preventDefault()
+    //the preventDefault prevents refresh upon click
+    let tripData = { 
+    dest: destination, 
+    depLoc: depLoc, 
+    startDate: startDate, 
+    returnDate: returnDate
+    };
+    setTripData(tripData);
+  }
+    
   return (
-    <div>
-      <p>This is the New Trips</p>
+    <div className="form-wrapper">
+      <form className='NewTripForm'>
+        <label for='depLoc'>Departure Location: </label>
+        <input type='text' id='depLoc' name='depLoc' value={depLoc} onChange={(event) => setDepLoc(event.target.value)}></input>
+        
+        <label for='startDate'>Departure Date: </label>
+        <input type='date' id='startDate' name='startDate' value={startDate} onChange={(event) => setStartDate(event.target.value)}></input><br></br><br></br>
+        
+        <label for='destination'>Destination Location: </label>
+        <input type='text' id='destination' name='destination' value={destination} onChange={(event) => setDestination(event.target.value)}></input>
+        
+        <label for='returnDate'>Return Date: </label>
+        <input type='date' id='returnDate' name='returnDate' value={returnDate} onChange={(event) => setReturnDate(event.target.value)}></input><br></br><br></br>
+        
+        <br></br>
+        <button type='submit' onClick={(event) => handleClick(event)}>Submit</button>
+      </form>
     </div>
   )
 }
-export default NewTrip
+export default NewTrip;
